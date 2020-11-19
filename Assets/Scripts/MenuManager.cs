5368e4fa9d01710ef29642d8c41cda3a617b432a
Samuel Bowman <bowmansa@iu.edu>
2020-11-18 23:15:12 -0500

    Final Milestone 1

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject creditsTextObject;
+
+    public void StartButtonPressed()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void CreditsButtonPressed()
+    {
+        creditsTextObject.SetActive(true);
+    }
+
+    public void QuitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            creditsTextObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] int rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(transform.up * rotationSpeed * Time.deltaTime);
+    }
+}
