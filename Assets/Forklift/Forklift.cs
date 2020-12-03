e3908ead455e269bb8aa2fb279dcd982b23bd10b
Samuel Bowman <bowmansa@iu.edu>
2020-12-03 00:40:17 -0500

    Final Milestone 2

--- Assets/Forklift/Forklift.cs
+++ Assets/Forklift/Forklift.cs
@@ -1,15 +1,6 @@
 ﻿using System;
 using System.Collections;
-using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
-enum GameState
-{
-    GAME_INTRO,
-    GAME_PLAY,
-    GAME_OVER,
-}
 
 public class Forklift : MonoBehaviour
 {
@@ -31,70 +22,24 @@ public class Forklift : MonoBehaviour
     Rigidbody rigidbody;
 
     [SerializeField] GameObject destroyables;
-    int objectsDestroyed = 0;
-    int totalDestroyables;
-    [SerializeField] GameObject destroyedCountTextObject;
-    TextMeshProUGUI destroyedCountText;
 
-    GameState gameState = GameState.GAME_INTRO;
-    float time = 0.0f;
-    [SerializeField] GameObject timerTextObject;
-    TextMeshProUGUI timerText;
-    [SerializeField] GameObject introText;
-    [SerializeField] GameObject gameOverText;
-    [SerializeField] GameObject timeTextObject;
-    TextMeshProUGUI timeText;
-    [SerializeField] GameObject thirdPersonCamera;
-    [SerializeField] GameObject firstPersonCamera;
+    LevelManager levelManager;
 
     void Start()
     {
+        levelManager = GameObject.Find("Level Manager").GetComponent<LevelManager>();
+
         frontLeftWheelCollider = frontLeftWheelColliderObject.GetComponent<WheelCollider>();
         frontRightWheelCollider = frontRightWheelColliderObject.GetComponent<WheelCollider>();
         rearLeftWheelCollider = rearLeftWheelColliderObject.GetComponent<WheelCollider>();
         rearRightWheelCollider = rearRightWheelColliderObject.GetComponent<WheelCollider>();
 
         rigidbody = GetComponent<Rigidbody>();
-
-        totalDestroyables = destroyables.transform.childCount;
-        destroyedCountText = destroyedCountTextObject.GetComponent<TextMeshProUGUI>();
-        UpdateDestroyedCount();
-
-        timerText = timerTextObject.GetComponent<TextMeshProUGUI>();
-        UpdateTimer();
-
-        timeText = timeTextObject.GetComponent<TextMeshProUGUI>();
-    }
-
-    private void Update()
-    {
-        if (Input.GetButtonDown("Submit"))
-        {
-            gameState = GameState.GAME_INTRO;
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-        if (Input.GetButtonDown("Toggle View"))
-        {
-            thirdPersonCamera.SetActive(!thirdPersonCamera.activeSelf);
-            firstPersonCamera.SetActive(!firstPersonCamera.activeSelf);
-        }
-        if (gameState == GameState.GAME_PLAY)
-        {
-            time += Time.deltaTime;
-            UpdateTimer();
-        }
-        else if (gameState == GameState.GAME_INTRO)
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
-                StartGame();
-            }
-        }
     }
 
     void FixedUpdate()
     {
-        if (gameState == GameState.GAME_PLAY)
+        if (levelManager.state == LevelState.PLAY)
         {
             if (Input.GetAxis("Vertical") != 0) {
                 if (rigidbody.velocity.magnitude < maxForwardVelocity)
@@ -122,51 +67,8 @@ public class Forklift : MonoBehaviour
     {
         if (other.gameObject.transform.parent != null && other.gameObject.transform.parent.gameObject == destroyables)
         {
-            //StartCoroutine(DestroyObject(other.gameObject));
             Destroy(other.gameObject);
-            objectsDestroyed++;
-            UpdateDestroyedCount();
-
-            if (objectsDestroyed == totalDestroyables)
-            {
-                GameOver();
-            }
+            levelManager.DestroyObject();
         }
-    }
-
-    IEnumerator DestroyObject(GameObject destroyedObject)
-    {
-        yield return new WaitForSeconds(2.0f);
-        Destroy(destroyedObject);
-        objectsDestroyed++;
-        UpdateDestroyedCount();
-
-        if (objectsDestroyed == totalDestroyables)
-        {
-            GameOver();
-        }
-    }
-
-    private void UpdateDestroyedCount()
-    {
-        destroyedCountText.text = "Destroyed: " + objectsDestroyed + "/" + totalDestroyables;
-    }
-
-    private void UpdateTimer()
-    {
-        timerText.text = "Time: " + ((int) time / 60).ToString("d2") + ":" + ((int) time % 60).ToString("d2");
-    }
-
-    private void StartGame()
-    {
-        introText.SetActive(false);
-        gameState = GameState.GAME_PLAY;
-    }
-
-    private void GameOver()
-    {
-        timeText.text = "Your Time: " + ((int)time / 60).ToString("d2") + ":" + ((int)time % 60).ToString("d2");
-        gameOverText.SetActive(true);
-        gameState = GameState.GAME_OVER;
     }
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum LevelState
+{
+    START,
+    PLAY,
+    FINSIH,
+}
+
+public class LevelManager : MonoBehaviour
+{
+    [HideInInspector] public LevelState state = LevelState.START;
+    int objectsDestroyed = 0;
+    int totalDestroyables;
+    float time = 0.0f;
+    [SerializeField] string nextLevelName;
+
+    [SerializeField] GameObject destroyables;
+    [SerializeField] GameObject destroyedCountTextObject;
+    TextMeshProUGUI destroyedCountText;
+    [SerializeField] GameObject timerTextObject;
+    TextMeshProUGUI timerText;
+    [SerializeField] GameObject introText;
+    [SerializeField] GameObject levelTextObject;
+    TextMeshProUGUI levelText;
+    [SerializeField] GameObject gameOverText;
+    [SerializeField] GameObject timeTextObject;
+    TextMeshProUGUI timeText;
+    [SerializeField] GameObject thirdPersonCamera;
+    [SerializeField] GameObject firstPersonCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalDestroyables = destroyables.transform.childCount;
+        destroyedCountText = destroyedCountTextObject.GetComponent<TextMeshProUGUI>();
+        UpdateDestroyedCount();
+
+        timerText = timerTextObject.GetComponent<TextMeshProUGUI>();
+        UpdateTimer();
+
+        levelText = levelTextObject.GetComponent<TextMeshProUGUI>();
+        levelText.text = SceneManager.GetActiveScene().name;
+
+        timeText = timeTextObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (state == LevelState.START)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                StartLevel();
+            }
+        }
+        else if (state == LevelState.PLAY)
+        {
+            
+            time += Time.deltaTime;
+            UpdateTimer();
+            if (Input.GetButtonDown("Toggle View"))
+            {
+                thirdPersonCamera.SetActive(!thirdPersonCamera.activeSelf);
+                firstPersonCamera.SetActive(!firstPersonCamera.activeSelf);
+            }
+            if (Input.GetButtonDown("Submit"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+        else if (state == LevelState.FINSIH)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                NextLevel();
+            }
+        }
+    }
+
+    void StartLevel()
+    {
+        state = LevelState.PLAY;
+        introText.SetActive(false);
+    }
+
+    void FinishLevel()
+    {
+        state = LevelState.FINSIH;
+        gameOverText.SetActive(true);
+        timeText.text = "Your Time: " + ((int)time / 60).ToString("d2") + ":" + ((int)time % 60).ToString("d2");
+    }
+
+    void NextLevel()
+    {
+        if (nextLevelName != "")
+        {
+            SceneManager.LoadScene(nextLevelName);
+        }
+        else
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+
+    public void DestroyObject()
+    {
+        objectsDestroyed++;
+        UpdateDestroyedCount();
+
+        if (objectsDestroyed == totalDestroyables)
+        {
+            FinishLevel();
+        }
+    }
+
+    void UpdateDestroyedCount()
+    {
+        destroyedCountText.text = "Destroyed: " + objectsDestroyed + "/" + totalDestroyables;
+    }
+
+    void UpdateTimer()
+    {
+        timerText.text = "Time: " + ((int) time / 60).ToString("d2") + ":" + ((int) time % 60).ToString("d2");
+    }
+}
